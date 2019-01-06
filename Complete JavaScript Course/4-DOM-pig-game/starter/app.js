b32021265b3856050ebb0a6a32570f86f585bb3d
Daynya Quigley <daynya@gmail.com>
2019-01-06 16:11:11 -0500

    Changes showed up in WP class folder.

--- Complete JavaScript Course/4-DOM-pig-game/starter/app.js
+++ Complete JavaScript Course/4-DOM-pig-game/starter/app.js
@@ -14,7 +14,7 @@ var scores, roundScore, activePlayer;
 
 scores = [0,0];
 roundScore = 0;
-activePlayer = 1;
+activePlayer = 0;
 
 document.querySelector('.dice').style.display = 'none';
 
@@ -35,9 +35,51 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
 	diceDOM.src = 'dice-' + dice + '.png';
  
 	// 3. update the round score if the rolled number is not a 1.
+	if (dice !== 1) {
+		//Add score
+		roundScore += dice;
+		document.querySelector('#current-' + activePlayer).textContent = roundScore;
+	} else {
+		//Next Player
+		nextPlayer();
+	}
+
 });
 
 
+document.querySelector('.btn-hold').addEventListener('click', function() {
+	//Add current score to global score
+	scores[activePlayer] += roundScore;
+	
+	//Update UI 
+	document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+
+	//Check if player won the game
+	if (scores[activePlayer] >= 10) {
+		document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
+		document.querySelector('.dice').style.display = 'none';
+		document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+		document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+	} else {
+		nextPlayer();
+	}
+});
+
+function nextPlayer() {
+	activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+	roundScore = 0;
+
+	document.getElementById('current-0').textContent = '0';
+	document.getElementById('current-1').textContent = '1';
+
+	document.querySelector('.player-0-panel').classList.toggle('active');
+	document.querySelector('.player-1-panel').classList.toggle('active');
+	
+	//document.querySelector('.player-0-panel').classList.remove('active');
+	//document.querySelector('.player-1-panel').classList.add('active');
+
+	document.querySelector('.dice').style.display = 'none';
+}
 
 //document.querySelector('#current-' + activePlayer).textContent = dice;
 //document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>'
