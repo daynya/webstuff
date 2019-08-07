949cd31b04e7a974fc1a4928f0b319ce857b0a7f
Daynya Quigley <daynya@gmail.com>
2019-08-07 18:05:29 -0400

    Events.

--- React For Beginners/React-For-Beginners-Starter-Files-master/catch-of-the-day/src/components/StorePicker.js
+++ React For Beginners/React-For-Beginners-Starter-Files-master/catch-of-the-day/src/components/StorePicker.js
@@ -2,25 +2,31 @@ import React from "react";
 import { getFunName } from "../helpers";
 
 class StorePicker extends React.Component {
-    goToStore(event) {
+    myInput = React.createRef();
+
+    goToStore = event => {
         // stop the form from submitting
         event.preventDefault();
         // get the text from that input
+        console.log(this.myInput.value);
+
         // change the page to/store/whatever-they-entered
-        console.log("going to store");
-    }
+
+    };
     render() {
         return (
             <form className="store-selector" onSubmit={this.goToStore}>
                 <h2>Please Enter A Store</h2>
                 <input 
                     type="text" 
-                    required placeholder="Store Name" 
+                    ref={this.myInput}
+                    required
+                    placeholder="Store Name" 
                     defaultValue={getFunName()}
                 />
                 <button type="submit">Visit Store →</button>
             </form>
-        )
+        );
     }
 }
 
