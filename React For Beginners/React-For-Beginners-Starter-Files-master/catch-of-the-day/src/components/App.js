a849ba1e2d82c550c03a5ec6dbaef6bc8049fa67
Daynya Quigley <daynya@gmail.com>
2019-09-08 14:11:13 -0400

    addToOrder function

--- React For Beginners/React-For-Beginners-Starter-Files-master/catch-of-the-day/src/components/App.js
+++ React For Beginners/React-For-Beginners-Starter-Files-master/catch-of-the-day/src/components/App.js
@@ -10,6 +10,7 @@ class App extends React.Component {
         fishes: {},
         order: {}
     };
+
     addFish = (fish) => {
         // 1. Take a copy of the existing state
         const fishes = { ...this.state.fishes };
@@ -19,17 +20,35 @@ class App extends React.Component {
         // adds fishes to previous const fishes to update to new fishes
         this.setState({ fishes });
     };
+
     loadSampleFishes = () => {
         this.setState({ fishes: sampleFishes });
-    }
+    };
+
+    addToOrder = key => {
+        // 1. take a copy of state
+        const order = { ...this.state.order };
+        // 2. either add to the order, or update the number in our order
+        // if the fish already exists, add another one. if it doesn't, add one.
+        order[key] = order[key] + 1 || 1;
+        // 3. call setState to update our state object
+        this.setState({ order });
+    }; 
+
     render() {
         return (
             <div className="catch-of-the-day">
                 <div className="menu">
                     <Header tagline="Fresh Seafood Market" />
                     <ul className="fishes">
-                        {Object.keys(this.state.fishes).map(key => <Fish key={key} details=
-                        {this.state.fishes[key]} />)}
+                        {Object.keys(this.state.fishes).map(key => (
+                        <Fish 
+                            key={key} 
+                            index={key}
+                            details={this.state.fishes[key]}
+                            addToOrder={this.addToOrder} 
+                        />
+                        ))}
                     </ul>
                 </div>
                 <Order />
